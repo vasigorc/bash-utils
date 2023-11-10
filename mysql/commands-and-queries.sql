edf27a10dd871e08c450fc51debbcf79fb33cd71
Vasile Gorcinschi <vasile.gorcinschi@improving.com>
2023-11-10 15:14:28 -0500

    :hammer: Add MySQL scripts

--- mysql/commands-and-queries.sql
+++ mysql/commands-and-queries.sql
@@ -3,3 +3,9 @@ SELECT column_name
 FROM information_schema.columns
 WHERE table_schema = 'company' 
   AND column_name LIKE '%name%';
+
+-- describe table
+SHOW CREATE TABLE <TABLE_NAME>;
+
+-- rebuild indices on a table
+ALTER TABLE <TABLE_NAME> FORCE;
