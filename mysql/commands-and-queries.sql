7cbe36ed62d164247c083e04392dbb75b8130d0c
Vasile Gorcinschi <vasile.gorcinschi@improving.com>
2024-02-15 09:30:26 -0500

    Changed sql queries

--- mysql/commands-and-queries.sql
+++ mysql/commands-and-queries.sql
@@ -1,5 +1,5 @@
 -- select all columns in a database that match 'name'
-SELECT column_name
+SELECT table_name, column_name
 FROM information_schema.columns
 WHERE table_schema = 'company' 
   AND column_name LIKE '%name%';
