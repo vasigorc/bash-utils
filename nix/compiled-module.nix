0098a081c140ca4040e3bd3f3e5d21fd8bf75b01
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-02-23 19:35:36 -0500

    chore: [nix] Added wasm and wasi targets to Rust toolchain

--- nix/compiled-module.nix
+++ nix/compiled-module.nix
@@ -16,5 +16,24 @@
   shellHook = ''
     # Ensure rustup is initialized
     rustup default stable
-  '';
+
+    # Add nightly toolchain if not present
+    if ! rustup toolchain list | grep -q "nightly"; then
+    rustup toolchain install nightly
+    fi
+
+    # Function to check and add target for a specific toolchain
+    check_and_add_target() {
+    local target=$1
+    local toolchain=$2
+    if ! rustup target list --installed --toolchain $toolchain | grep -q "^$target"; then
+    echo "Adding target $target for $toolchain toolchain..."
+    rustup target add $target --toolchain $toolchain
+    fi
+    }
+
+    # Add required targets for nightly toolchain if not present
+    check_and_add_target "wasm32-unknown-unknown" "nightly"
+    check_and_add_target "wasm32-wasi" "nightly"
+    '';
 }
