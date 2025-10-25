c16a767b4242e7a291bde6aadefb5cd431936fd1
vasigorc <vasigorc@gmail.com>
2025-10-25 18:57:11 -0400

    chore: [nix] Updated jvm-module.nix
    Added support for Kotlin and Gradle. Upgraded Temurin JDK 17 -> 21

--- nix/jvm-module.nix
+++ nix/jvm-module.nix
@@ -3,7 +3,7 @@
 {
   buildInputs = with pkgs; [
     # JDK
-    temurin-bin-17
+    temurin-bin-21
 
     # Scala ecosystem
     scala_2_13
@@ -11,15 +11,24 @@
     coursier
     metals
 
+    # Kotlin ecosystem
+    kotlin
+    vimPlugins.nvim-treesitter-parsers.kotlin
+    detekt
+    ktfmt
+    ktlint
+
+    gradle
+
   ];
 
   shellHook = ''
-      # Set JAVA_HOME explicitly
-      export JAVA_HOME=${pkgs.temurin-bin-17}
+    # Set JAVA_HOME explicitly
+    export JAVA_HOME=${pkgs.temurin-bin-21}
 
-      # Add coursier bin directory to PATH
-      export PATH=$PATH:$HOME/.local/share/coursier/bin
+    # Add coursier bin directory to PATH
+    export PATH=$PATH:$HOME/.local/share/coursier/bin
 
-      echo "JVM development environment ready with Temurin JDK 17, Scala 2.13, and SBT"
+    echo "JVM development environment ready with Temurin JDK 21, Scala 2.13, and SBT"
   '';
 }
