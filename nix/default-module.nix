5d0cdbb428aa7f690e706811a1483a91d0af4e57
name <email>
2026-01-19 12:01:30 -0500

    nix(default): Added useful CLI tools
    - git-extras
    fzf-git
    -bat

--- nix/default-module.nix
+++ nix/default-module.nix
@@ -1,19 +1,22 @@
 { pkgs }:
 
 {
-    buildInputs = with pkgs; [
-        # Shell and utilities
-        zsh
-        oh-my-zsh
-        jq
-        git
-        fzf
-        silver-searcher
-        ripgrep # similar to silver-search but integrates well with vim/nvim
-    ];
+  buildInputs = with pkgs; [
+    # Shell and utilities
+    zsh
+    bat
+    git-extras
+    oh-my-zsh
+    jq
+    git
+    fzf
+    fzf-git-sh
+    silver-searcher
+    ripgrep # similar to silver-search but integrates well with vim/nvim
+  ];
 
-    shellHook = ''
-        # Add any nix-shell specific configurations here
-        export GIT_PAGER=
-    '';
+  shellHook = ''
+    # Add any nix-shell specific configurations here
+    export GIT_PAGER=
+  '';
 }
